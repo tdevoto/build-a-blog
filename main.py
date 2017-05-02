f5d07826a0cb33dc28c2c11f02efeedc1611449e
Tony Devoto <tonydevoto@gmail.com>
2017-05-01 20:50:12 -0500

    started the code

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+
+class Blogpost(db.Model):
+	title = db.StringProperty(required = True)
+	entry = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+
+
+class MainHandler(Handler):
+
+	def render_front(self, title="", entry="", error=""):
+		blogposts = db.GqlQuery("select * from Blogpost order by created desc")
+		self.render("main.html", title=title, entry=entry, error=error, blogposts=blogposts)
+
+	def get(self):
+		self.render_front()
+
+	def post(self):
+		title = self.request.get("title")
+		entry = self.request.get("entry")
+
+		if title and entry:
+			a = Blogpost(title=title, entry=entry)
+			a.put()
+
+			self.redirect("/")
+		else:
+			error = "we need both title and a new entry"
+			self.render_front(title, entry, error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+	('/', MainHandler)
 ], debug=True)
